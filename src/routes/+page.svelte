4fcac562d9aec76da62021f45541da08786d89b1
Le Pham <phamtle724@gmail.com>
2023-05-14 12:45:07 -0700

    add filter function, need to handle no user message while loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { cacheExchange, createClient, fetchExchange, gql } from '@urql/svelte';
-	import { onMount, onDestroy } from 'svelte';
+	import { onMount } from 'svelte';
 
 	import Loader from 'components/Loader.svelte';
 	import User from 'components/User.svelte';
@@ -19,12 +19,13 @@
 	let isLoading: boolean = false;
 	let hasMore: boolean = true;
 	let timeoutId: number;
+  let searchQuery:string = ""
 
 	async function fetchUsers() {
     console.log("fetching users")
 		const query = gql`
-			query ($first: Int, $after: String) {
-				users(first: $first, after: $after) {
+			query ($first: Int, $after: String, $name: String) {
+				users(first: $first, after: $after, name: $name) {
 					id
 					name
 					avatar
@@ -35,7 +36,7 @@
 		isLoading = true;
 
 		const { data } = await client
-			.query(query, { first: USER_BATCH_SIZE, after: after.toString() })
+			.query(query, { first: USER_BATCH_SIZE, after: after.toString(), name: searchQuery })
 			.toPromise();
 		isLoading = false;
 		if (data && data.users.length > 0) {
@@ -47,15 +48,18 @@
 		}
 	}
 
+  function searchUsers() {
+    users = [];
+    after = 0;
+    fetchUsers();
+  }
+
 	function fetchUsersWithTimeout() {
 		isLoading = true;
 		timeoutId = window.setTimeout(fetchUsers, 1000);
 	};
 
 	onMount(fetchUsers);
-	onDestroy(() => {
-		clearTimeout(timeoutId);
-	});
 
 	function nextBatch() {
 		after = users.length;
@@ -65,11 +69,14 @@
 	}
 </script>
 
+<div>
+  <input type="text" bind:value="{searchQuery}"/>
+  <button on:click="{searchUsers}">Search</button>
 <div class="w-full h-full overflow-scroll">
 	<div class="flex flex-col gap-4 items-center p-4 h-full w-full overflow-scroll">
-		{#if users.length === 0}
+		{#if users.length === 0 && !isLoading}
 			<div class="flex items-center justify-center">
-				<Loader />
+				No users found.
 			</div>
 		{:else}
 			{#each users as user (user.id)}
@@ -88,4 +95,5 @@
 			{/if}
 		{/if}
 	</div>
+</div>
 </div>
--- src/routes/graphql/+server.ts
+++ src/routes/graphql/+server.ts
@@ -14,10 +14,11 @@ const yogaApp = createYoga<RequestEvent>({
 		resolvers: {
 			Query: {
 				users: (source, args, context, info) => {
-					const { first = USER_BATCH_SIZE, after} = args;
+					const { first = USER_BATCH_SIZE, after, name = ""} = args;
 
+					const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(name.toLowerCase()));
 					const startIdx = after !== 0 ? parseInt(after, 10) : 0;
-					const paginatedUsers = users.slice(startIdx, startIdx + first)
+					const paginatedUsers = filteredUsers.slice(startIdx, startIdx + first)
 					console.log("paginated users", paginatedUsers)
 					return paginatedUsers
 				}
